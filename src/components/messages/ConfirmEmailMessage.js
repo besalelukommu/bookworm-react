1ca58b9a80fdf84cc312f0558ef36916a5c98583
besi <besalelu.php@gmail.com>
2021-02-19 03:06:09 +0530

    authorised routes

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react'
+
+
+export default function ConfirmEmailMessage() {
+    return (
+        <Message info>
+            <Message.Header>Please, verify your email to unlock you account</Message.Header>
+            
+        </Message>
+    )
+}
---
+++ src/components/routes/GuestRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+
+const UserRoute = ({isAuthenticated, component: Component, ...rest }) => (
+    <Route {...rest} render={props => !isAuthenticated ? <Component {...props} /> : <Redirect to="/dashboard" />} />
+);
+UserRoute.propTypes = {
+    component: PropTypes.func.isRequired,
+    isAuthenticated:PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.token
+    }
+}
+export default connect(mapStateToProps)(UserRoute);
